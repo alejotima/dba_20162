410339debc1f792a0aefb848c2765f3765d8aa81
Manuel Ramirez <alejotima@gmail.com>
2016-11-01 15:17:24 -0500

    proyecto 1

--- Static/JS/main.js
+++ Static/JS/main.js
@@ -1,14 +1,45 @@
-var app = angular.module('myApp', ['ngRoute']);
+var app = angular.module('myApp', ['ngRoute', 'ngResource']);
+app.factory('Api', function($resource){
+    return {
+        User: $resource('api/users/:id',{
+            id: '@id'
+        })
+    }
+});
 app.config(function ($routeProvider) {
     $routeProvider
     .when('/home', {
-        templateUrl: 'localhost:5000/static/home.html'
+        templateUrl: '/static/views/home.html'
     })
-    .when('/test', {
-        templateUrl: '/static/home.html'
+    .when('/crud', {
+        templateUrl: '/static/views/crud.html'
     })
 });
 app.controller('myCtrl', function ($scope) {
     $scope.firstName = "John";
     $scope.lastName = "Doe";
+});
+
+app.controller('CrudCtrl', function ($scope, Api) {
+    $scope.test = 'Esto es un prueba';
+    $scope.users = Api.User.query();
+    $scope.addUser = function(){
+        console.log('esto es un agregar');
+        $scope.newUser = {
+            name: $scope.name,
+            lastname: $scope.lastname
+        };
+        console.log($scope.newUser);
+        Api.User.save($scope.newUser, function(data) {
+            console.log(data);
+            $scope.users = Api.User.query();
+        });
+    };
+    $scope.removeUser = function(idnt){
+        console.log('esete es el id a borrar - '+idnt);
+        Api.User.delete({id:idnt},function(data){
+            console.log(data);
+            $scope.users = Api.User.query();
+        });
+    };
 });
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import cx_Oracle
 import os
 
@@ -9,7 +9,7 @@ def get_all_users():
 	try:
 		cur = conn.cursor()
 		try:
-			cur.execute('''SELECT id, name, lastname FROM usuario''')
+			cur.execute('''SELECT id, name, lastname FROM test1''')
 			result = []
 			for identificador, name, lastname in cur:
 				result.append({'id': identificador,'name':name,'lastname': lastname})
@@ -24,11 +24,33 @@ def get_all_users():
 
 @app.route('/api/users', methods=["POST"])
 def insert_user():
-	return 'esto es un post'
+    try:
+        cur = conn.cursor()
+        try:
+            data = request.json
+            print data
+            statement = 'insert into test1(name,lastname) values (:2,:3)'
+            cur.execute(statement,(request.json['name'], request.json['lastname']))
+            conn.commit()
+            return jsonify(True)
+        finally:
+            cur.close()
+    finally:
+        print 'Se inserto'
 
 @app.route ('/api/users/<int:id>', methods=["DELETE"])
 def delete_user(id):
-	return 'esto es un delete - '+str(id)
+    try:
+        cur = conn.cursor()
+        try:
+            statement = 'delete from test1 where id = :id'
+            cur.execute(statement,{'id':id})
+            conn.commit()
+            return jsonify(True)
+        finally:
+            cur.close()
+    finally:
+        print 'Se borro'
 
 @app.route ('/')
 def home():
@@ -36,8 +58,9 @@ def home():
 
 
 if __name__=="__main__":
-	conn = cx_Oracle.connect('sys', '12345678', 'localhost:1521/orcl', mode = cx_Oracle.SYSDBA)
-	os.environ['NLS_LANG']='.UTF8'
+    conn = cx_Oracle.connect('C##HR','12345', 'localhost:1521/orcl')
+	#conn = cx_Oracle.connect('sys', '12345678', 'localhost:1521/orcl', mode = cx_Oracle.SYSDBA)
+    os.environ['NLS_LANG']='.UTF8'
 	#host="0.0.0.0" Para hacer que nuestro servidor sea visible, publico en la red privada
 	#port=5000 puerto 5000
-	app.run(debug=True,host="0.0.0.0", port=5000)
+    app.run(debug=True,host="0.0.0.0", port=5000)
